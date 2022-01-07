5a352b9cb18f063282d2e36f0bb04b268db6190e
Vivek Nair <vivek.nair@condenast.com>
2022-01-07 11:00:20 +0000

    add dependency

---
+++ go.mod
@@ -0,0 +1,5 @@
+module sub_download
+
+go 1.17
+
+require github.com/viveknair00/scaling-telegram v0.0.1
---
+++ sub_download.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/viveknair00/scaling-telegram/download"
+)
+
+func main() {
+	fmt.Println(download.Add(1, 2))
+}
